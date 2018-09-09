4b678485bc4dd082244bd6b1ffb741a3620cf2f2
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-09 17:26:06 -0500

    created Song class

--- KillNickelback/Program.cs
+++ KillNickelback/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace KillNickelback
 {
@@ -6,7 +7,37 @@ namespace KillNickelback
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var goodSongs = new List<Song>();
+
+            var allSongs = new HashSet<Song>();
+
+            allSongs.Add(new Song("Lynyrd Skynard", "Simple Man"));
+            allSongs.Add(new Song("Foghat", "Slow Ride"));
+            allSongs.Add(new Song("Elton John", "Funeral for a Friend"));
+            allSongs.Add(new Song("Father John Misty", "Now I'm Learning to Love the War"));
+            allSongs.Add(new Song("Nickelback", "Photograph"));
+            allSongs.Add(new Song("Nickelback", "How You Remind Me"));
+            allSongs.Add(new Song("Nickelback", "Rockstar"));
+
+            foreach (var song in allSongs)
+            {
+
+            }
+
+        public class Song
+        {
+            public string Artist { get;  }
+            public string Name { get; }
+            public Song(string artist, string name)
+            {
+                Artist = artist;
+                Name = name;
+            }
+        }
+
+
+
+
         }
     }
 }
