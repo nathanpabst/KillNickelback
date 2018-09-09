cce62ac8442d259e1c5ab7f792a158d9967c17fe
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-09 17:15:31 -0500

    goodSongs list and allSongs HashSet created

---
+++ KillNickelback/Song.cs
@@ -0,0 +1,14 @@
+﻿namespace KillNickelback
+{
+    internal class Song
+    {
+        private string v1;
+        private string v2;
+
+        public Song(string v1, string v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+    }
+}
